804653b012c459612eb1b6d3f0ce9a7605d46420
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2021-04-11 13:53:11 +0200

    test

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
+  target: "experimental-serverless-trace",
+}
